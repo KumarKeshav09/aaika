f9d1826fb4974a2b6b1ebd8b7f5813f175f7697e
kumarkeshav09 <kumarkeshavkashyap7@gmail.com>
2024-11-13 11:03:10 +0530

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
- 
 import Footer from "@/components/Footer";
 import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navbar from "@/components/NavBar";
+import Link from "next/link";
 
 export const metadata = {
   title: "Aaika",
@@ -13,11 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {/* <Navbar /> */}
-        {children}
-        {/* <Footer /> */}
-      </body>
+      <head>
+        <link rel="icon" href="/images/logo.png" type="image/x-icon"></link>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
